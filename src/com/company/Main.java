7b26586aac4598bb46cc623c1ae1259aa42c18ba
lcy <lcyking@163.com>
2019-09-23 11:21:43 +0800

    20190923

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,33 +25,7 @@ import java.util.Map;
 
 public class Main {
 
-
-    public static void main(String[] args)  {
-
+    public static void main(String[] args) {
         System.out.println(PropsUtil.renameToUUID("123.sa"));
-
-
-
-
-
-    public static void main(String[] args)  {
-         //  System.out.println(SafeString.encode("123"));
-/*        Div div = new Div();
-        Table table = div.table();
-        int count = 15;
-        int MAX_COl = 5;
-        TBody tBody = table.tbody();
-
-        TableRow tr = null;
-        for (int i = 0, j = 0; i < count; i++, j++) {
-            if (j % MAX_COl == 0) {
-                tr = tBody.tr();
-            }
-            TableDataCell tableRow = tr.td();
-            tableRow.styles("text-align:center; font-szie:18px;");
-            tableRow.img("green.jpg");
-            tableRow.p("文字:" + i);
-        }
-        System.out.println(div);*/
     }
 }
