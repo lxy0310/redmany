0c652f2b236e0f1ebca11f5407cd91855734b8b6
AriosZero <289241018@qq.com>
2019-11-21 14:27:28 +0800

    测试上传20191121

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,6 +26,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(1);
         System.out.println(PropsUtil.renameToUUID("123.sa"));
     }
 }
